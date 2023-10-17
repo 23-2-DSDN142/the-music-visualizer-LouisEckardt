fed55195d25ae56666f78376247885f294cc045e
LouisEckardt <eckardloui@myvuw.ac.nz>
2023-10-17 13:53:11 +1300

    idk

--- music_starter.js
+++ music_starter.js
@@ -1,68 +1,34 @@
-
-//vocal, drum, bass, and other are volumes ranging from 0 to 100
-    
-let firstRun = true
-let speaker;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  if(firstRun){
-    rectMode(CENTER);
-    speaker = loadImage('Untitled_Artwork.png');
+  if(bass < 20){
+  background(20)}
+  else{
+    background(200)
 
-    firstRun = false
-  }
+  } 
+  textFont('Helvetica'); // please use CSS safe fonts
+  rectMode(CENTER);
 
+  let blue = color(3, 86, 252);
+  let green = color(45, 252, 3);
+  let mapForColorLerp = map(vocal, 0, 100, 0, 1);
+  let mappedColour = lerpColor(blue, green, mapForColorLerp);
+  let rectSize = map(drum, 0, 100, 100, 200);
+  let circleYOffset = map(other, 0, 100, -100, 100);
  
-  
-  image(speaker, 100,80)
+  fill(255) // circle to stay white
+  rect(width / 2 , height / 2, rectSize, rectSize, 1, 1)
+
+
+  let textBackgroundSize = map(words.length, 0, 12, 0, width / 2)
+
+  // display "words"
+  fill(mappedColour)
+  textAlign(CENTER);
+  rect(width / 2, height / 3, textBackgroundSize, 100)
+  fill(255)
+  // textSize(vocal);
+  // text(words, width / 2, height / 3);
+
 
 }
-// function draw_one_frame(words, vocal, drum, bass, other, counter) {
-//       fill(255);
-//       textFont('Helvetica');
-//       textAlign(CENTER);
-//       textSize(vocal);
-//       text(words, width / 2, height / 3);
-      
-//       const ballSize = 40;
-//       const drumHeight = map(drum, 0, 255, 0, height);
-//       fill(244, 161, 39);
-//       ellipse(width / 2, drumHeight, ballSize + bass, ballSize + bass);
-    
-//       fill(bass, other, counter);
-//       ellipse(width / 2, height / 2, 50 + bass, 50 + vocal);
-//     }
-    
-   
-
-  //  let bar_spacing = height / 10;
-  //  let bar_height = width / 12;
-  //  let bar_pos_x = width / 2;
- 
-
-  //  // vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
- 
-  
