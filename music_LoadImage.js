6e38c89123f1685defdb399af736228f03fd2f90
LouisEckardt <141703969+LouisEckardt@users.noreply.github.com>
2023-10-21 16:10:40 +1300

    bunny image

--- music_LoadImage.js
+++ music_LoadImage.js
@@ -14,3 +14,38 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   image(testImg, 100,80)
 
 }
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  if(bass < 20){
+  background(0, 128, 0)}
+  else{
+    background(200)
+
+  } 
+  textFont('Helvetica'); // please use CSS safe fonts
+  rectMode(CENTER);
+
+  let blue = color(3, 86, 252);
+  let green = color(45, 252, 3);
+  let mapForColorLerp = map(vocal, 0, 100, 0, 1);
+  let mappedColour = lerpColor(blue, green, mapForColorLerp);
+  let rectSize = map(drum, 0, 100, 100, 100,);
+  let circleYOffset = map(other, 0, 100, -100, 100);
+ 
+  fill(255) // circle to stay white
+  rect(width / 2 , height / 2, rectSize, rectSize, 1, 1)
+
+
+  let textBackgroundSize = map(words.length, 0, 12, 0, width / 2)
+
+  // display "words"
+  fill(mappedColour)
+  textAlign(CENTER);
+  rect(width / 2, height / 3, textBackgroundSize, 100)
+  fill(255)
+  // textSize(vocal);
+  // text(words, width / 2, height / 3);
+
+
+  
+}
--- music_starter.js
+++ music_starter.js
@@ -1,35 +1,25 @@
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  if(bass < 20){
-  background(255, 0, 0)}
-  else{
-    background(200)
+let fisrtRun = true
 
-  } 
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER);
+let Bunny = [];
 
-  let blue = color(3, 86, 252);
-  let green = color(45, 252, 3);
-  let mapForColorLerp = map(vocal, 0, 100, 0, 1);
-  let mappedColour = lerpColor(blue, green, mapForColorLerp);
-  let rectSize = map(drum, 0, 100, 100, 100,);
-  let circleYOffset = map(other, 0, 100, -100, 100);
- 
-  fill(255) // circle to stay white
-  rect(width / 2 , height / 2, rectSize, rectSize, 1, 1)
+function draw_one_frame(words, vocal, drum, bass, other,counter) {
+  if(firstRun)  {
+    rectMode(CENTER);
+    Bunny.push(loadImage('bunnyImage_0.png'));
+    Bunny.push(loadImage('bunnyImage_1.png'));
+    
 
+    
+    fisrtRun = false
+  }
+  background(255);
 
-  let textBackgroundSize = map(words.length, 0, 12, 0, width / 2)
-
-  // display "words"
-  fill(mappedColour)
-  textAlign(CENTER);
-  rect(width / 2, height / 3, textBackgroundSize, 100)
-  fill(255)
-  // textSize(vocal);
-  // text(words, width / 2, height / 3);
+var VocalFrame = int(map(vocal, 0, 100, 0, 2));
+push();
+scale(0.2);
+image(Bunny[VocalFrame], width/2, height/2)
+pop();
 
 
-  
-}
+
+  }
